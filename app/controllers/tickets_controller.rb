a5d3b89086eef37ea8f2d975b0d1fd30e55e1ca2
Sam Mills <s.mills131@gmail.com>
2013-09-12 12:32:19 -0400

    fixed saving of technicians on jobs
    added the ticket id array to strong params

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -45,7 +45,7 @@ class TicketsController < ApplicationController
 #Private Actions	
 private	
 	def ticket_params
-		params.require(:ticket).permit(:user_id, :asset_id, :title, :description, :status, :deadline, :priority)
+		params.require(:ticket).permit(:user_id, :asset_id, :title, :description, :status, :deadline, :priority, :technician_ids, {:technician_ids => []})
     end
 	
 end
