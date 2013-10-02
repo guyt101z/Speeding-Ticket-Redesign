80ed8bd82c01fbcbc94f73ec6628c96fd07e445f
Sam Mills <s.mills131@gmail.com>
2013-10-02 16:55:54 -0400

    fix compass rails again working now.

---
+++ vendor/bundle/bundler/gems/compass-rails-32c16cb90f78/lib/compass-rails/railties.rb
@@ -0,0 +1,13 @@
+if defined?(::Rails)
+  if CompassRails.rails2?
+    require "compass-rails/railties/2_3"
+  elsif CompassRails.rails31? || CompassRails.rails32?
+    require "compass-rails/railties/3_1"
+  elsif CompassRails.rails3?
+    require "compass-rails/railties/3_0"
+  elsif CompassRails.rails4?
+    require "compass-rails/railties/4_0"
+  else
+    $stderr.puts "Unsupported rails environment for compass"
+  end
+end
