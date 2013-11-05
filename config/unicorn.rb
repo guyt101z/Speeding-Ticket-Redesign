a83d4c3b9283cf5dcb220e230a0caa51299cd570
Sam Mills <s.mills131@gmail.com>
2013-11-05 16:02:37 -0500

    Revert "capistrano and unicorn stuff"
    This reverts commit 4f91c882832479d1caa9567ff31134d3fd628898.

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/root/speedingTicketNew/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.speedingTicketNew.sock"
-worker_processes 2
-timeout 30
